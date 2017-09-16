20bfefddcdd9ef216df7513efeea5f178dbf39f9
Faiz Muhammad Refano <faizrefano@Faizs-MacBook-Pro.local>
2017-09-16 21:40:35 +0700

    push pertama without writeup

---
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,56 @@
+package com.example.demo.controller;
+	import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+	import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+	import org.springframework.web.bind.annotation.RequestParam;
+
+	@Controller
+public class PageController {
+	@RequestMapping("/hello")
+	public String index()
+	{
+		return "hello";
+	}
+	@RequestMapping("/greeting")
+	public String greeting(@RequestParam(value = "name" , required = false , defaultValue ="dunia")String name , Model model)
+	{
+		model.addAttribute("name",name);
+		return "greeting";
+	}
+	@RequestMapping(value= {"/greeting","/greeting/{name}"})
+	public String greetingPath(@PathVariable Optional<String>name , Model model) {
+		if(name.isPresent()) {
+			model.addAttribute("name",name.get());
+		}else {
+			model.addAttribute("name","dengklek");
+		}
+		return "greeting";
+	}
+	@RequestMapping("/latihan2")
+	public String perkalian(@RequestParam(value = "a",required = false) String a ,@RequestParam(value = "b" , required = false) String b , Model model )
+	{
+		
+		
+		if(a==null && b==null) {
+			model.addAttribute("a",0);
+			model.addAttribute("b",0);
+		}
+		else if(a == null) {
+			model.addAttribute("a", 0);
+			model.addAttribute("b", b);
+		}
+		else if(b==null) {
+			model.addAttribute("a", a);
+			model.addAttribute("b", 0);
+		}
+		else {
+			model.addAttribute("a",a);
+			model.addAttribute("b",b);
+		}
+		return "latihan2";
+	
+	}
+}
